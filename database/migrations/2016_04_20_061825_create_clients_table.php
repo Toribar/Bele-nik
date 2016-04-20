d3e540708fe2742d5bf3249602695b666c2e5f97
Bata <bata.henry@gmail.com>
2016-04-20 08:35:03 +0200

    laravel collective

---
+++ database/migrations/2016_04_20_061825_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('serial')->index();
+             $table->date('created_on');
+            $table->text('info');
+            $table->string('public_book')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+    }
+}
